6cdca0ab21a4b6ef0528106abd2724ec5fcce8e6
Barker1889 <barker220489@gmail.com>
2015-03-28 20:17:51 +0000

    AngularUpdate: Restarts the singalR hub on the dashboard after disconnecting.

--- MvcMonitor.WebApp/Client/ApplicationErrorSummaryCtrl.js
+++ MvcMonitor.WebApp/Client/ApplicationErrorSummaryCtrl.js
@@ -1,45 +1,45 @@
 ﻿app.controller('ApplicationErrorSummaryCtrl', function($scope) {
+    var signalrConnectionStates = { 0: 'Connecting', 1: 'Connected', 2: 'Reconnecting', 4: 'Disconnected' };
 
-    $(function() {
-        var proxy = $.connection.showcaseErrorHub;
+    var proxy = $.connection.showcaseErrorHub;
 
-        function init() {
-            proxy.server.getApplicationErrorSummary().done(function(summary) {
-                $scope.$apply(function() {
-                    $scope.applicationsSummary = summary;
-                });
-            });
-
-            proxy.server.getErrorLocations().done(function (locationCount) {
-                $scope.$apply(function() {
-                    $scope.errorLocations = locationCount;
-                });
-            });
-        }
-
-        proxy.client.errorReceived = function (error) {
-            init();
-        };
-
-        function connectionStateChanged(state) {
-            var connectionStates = { 0: 'Connecting', 1: 'Connected', 2: 'Reconnecting', 4: 'Disconnected' };
-
-            $scope.connectionState = connectionStates[state.newState];
-
+    function refresh() {
+        proxy.server.getApplicationErrorSummary().done(function(summary) {
             $scope.$apply(function() {
-                if (state.newState == 1) {
-                    $scope.connectionErrorDisplay = false;
-                } else {
-                    $scope.connectionErrorDisplay = true;
-                }
+                $scope.applicationsSummary = summary;
             });
+        });
+
+        proxy.server.getErrorLocations().done(function (locationCount) {
+            $scope.$apply(function() {
+                $scope.errorLocations = locationCount;
+            });
+        });
+    }
+
+    proxy.client.errorReceived = function (error) {
+        refresh();
+    };
+
+    function connectionStateChanged(state) {
+        console.log('Connection state changed to ' + signalrConnectionStates[state.newState]);
+
+        if (state.newState == 4) {
+            console.log('Restarting hub...');
+            connectToHub();
         }
 
+        $scope.connectionState = signalrConnectionStates[state.newState];
+    }
+
+    function connectToHub() {
         try {
-            proxy.connection.stateChanged(connectionStateChanged);
-            $.connection.hub.start().done(init);
-        } catch(e) {
+            $.connection.hub.start().done(refresh);
+        } catch (e) {
             alert(e.message);
         }
-    });
+    }
+
+    proxy.connection.stateChanged(connectionStateChanged);
+    connectToHub();
 });
