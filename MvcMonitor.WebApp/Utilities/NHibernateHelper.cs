88305885c0b991d3328d052cf77c135a42271221
Tommy Ly <tommyly764@gmail.com>
2015-10-04 12:58:23 +0100

    touch

--- MvcMonitor.WebApp/Utilities/NHibernateHelper.cs
+++ MvcMonitor.WebApp/Utilities/NHibernateHelper.cs
@@ -24,7 +24,7 @@ namespace MvcMonitor.Utilities
 
         public static ISession OpenSession()
         {
-            return SessionFactory.OpenSession();
+            return SessionFactory.OpenSession(); 
         }
     }
 }
